4a07652e04c2de90c30ffd2f4e963e8d5b1815bb
alaguna <rodrigoalagunamoreno@gmail.com>
2020-05-07 01:33:25 -0500

    agregue rutas y controladores

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express')
+const express = require('express');
+const routes = require('./routes');
 const PORT = process.env.PORT || 5000
 
-express()
-  .get('/', function(req, res) {
-  res.send("Hello World!");
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express()
+  app.use('/',routes());
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
